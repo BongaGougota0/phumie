be98c70ca40fe4908749fef54187c701c88dcefa
BongaGougota0 <bonga_gougota@outlook.com>
2026-04-11 13:09:05 +0200

    use java-17.

--- phumie-microservices-project/PostsService/src/main/java/za/co/phumie/PostsService/repository/CommentsRepository.java
+++ phumie-microservices-project/PostsService/src/main/java/za/co/phumie/PostsService/repository/CommentsRepository.java
@@ -1,8 +1,9 @@
 package za.co.phumie.PostsService.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.co.phumie.PostsService.model.Comment;
 
 @Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentsRepository extends JpaRepository<Comment, Long> {
 }
