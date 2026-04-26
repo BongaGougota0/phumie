6db8b88f3bce7aa9d1a447d3dc8ff11010e7746f
BongaGougota0 <bonga_gougota@outlook.com>
2026-04-27 00:37:08 +0200

    update stop/

--- phumie-microservices-project/stop_services.sh
+++ phumie-microservices-project/stop_services.sh
@@ -2,7 +2,7 @@
 
 echo "Stopping all services..."
 
-pkill -f "service/build/libs"
+pkill -f "service_registry_application-0.0.1-SNAPSHOT.war"
 pkill -f "api-gateway-0.0.1-SNAPSHOT.war"
 pkill -f "LikeAPI-0.0.1-SNAPSHOT.war"
 pkill -f "PostService-0.0.1-SNAPSHOT.jar"
